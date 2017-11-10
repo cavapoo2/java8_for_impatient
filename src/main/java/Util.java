d758fa2f6f2e23ee55463a2ae49bf6030002b176
andyr <forpetessake@protonmail.com>
2017-11-10 10:29:02 +0000

    Add files via upload

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,11 +1,12 @@
 package com.andyr.impatient;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public interface Util {
 
-    void action();
+     void action();
 
     default List<String> getWordsAsList(String name) {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
@@ -13,7 +14,17 @@ public interface Util {
         String res = Util.getStringFromInputStream(is);
         return Arrays.asList(res.split("[\\P{L}]+"));
     }
-
+    default ArrayList<String> getWordsAsArrayList(String name) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream(name);
+        String res = Util.getStringFromInputStream(is);
+        //return new ArrayList<String>(Arrays.asList(res.split("[\\P{L}]+")));
+        //return new ArrayList<String>(Arrays.asList(res.split("[\\n\\p{Punct}\\s]+")));
+        //System.out.println(res);
+       // String next = res.replaceAll("(\t|\r?\n)+", " ");
+        //System.out.println(next);
+        return new ArrayList<String>(Arrays.asList(res.split("\\P{L}+")));
+    }
     default String[] getWordsAsArray(String name) {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream(name);
@@ -37,7 +48,7 @@ public interface Util {
 
             br = new BufferedReader(new InputStreamReader(is));
             while ((line = br.readLine()) != null) {
-                sb.append(line);
+                sb.append(line + "\n");//add bug fix, needs new line appending
             }
 
         } catch (IOException e) {
@@ -54,5 +65,6 @@ public interface Util {
         return sb.toString();
 
     }
+
 
 }
